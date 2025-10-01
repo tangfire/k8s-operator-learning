3a316496b850a849ec4957a2dabe47a7d73a2c5e
tangfire <52628216@qq.com>
2025-10-01 21:02:52 +0800

    first commit

---
+++ 02-Clientset/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	coreV1 := clientset.CoreV1()
+	pod, err := coreV1.Pods("default").Get(context.TODO(), "test", v1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pod.Name)
+
+}
