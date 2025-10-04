1ad5fe2080c8fab1593cd32a07b5c1fc508f3963
tangfire <52628216@qq.com>
2025-10-04 19:00:13 +0800

    测试验证

--- 05-Demo/pkg/controller.go
+++ 05-Demo/pkg/controller.go
@@ -1,9 +1,13 @@
 package pkg
 
 import (
+	"context"
+	v4 "k8s.io/api/core/v1"
 	v2 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	v3 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	informer "k8s.io/client-go/informers/core/v1"
 	netInformer "k8s.io/client-go/informers/networking/v1"
 	"k8s.io/client-go/kubernetes"
@@ -12,6 +16,12 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"reflect"
+	"time"
+)
+
+const (
+	workNum  = 5
+	maxRetry = 10
 )
 
 type controller struct {
@@ -55,8 +65,143 @@ func (c *controller) deleteIngress(obj interface{}) {
 }
 
 func (c *controller) Run(stopCh chan struct{}) {
-
+	for i := 0; i < workNum; i++ {
+		go wait.Until(c.worker, time.Minute, stopCh)
+	}
 	<-stopCh
+}
+
+func (c *controller) worker() {
+	for c.processNextItem() {
+
+	}
+}
+
+func (c *controller) processNextItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Done(item)
+	key := item.(string)
+	err := c.syncService(key)
+	if err != nil {
+		c.handlerError(key, err)
+	}
+	return true
+
+}
+
+func (c *controller) syncService(key string) error {
+	namespaceKey, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+
+	// 删除
+	service, err := c.serviceLister.Services(namespaceKey).Get(name)
+	if errors.IsNotFound(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	// 新增和删除
+	_, ok := service.GetAnnotations()["ingress/http"]
+	ingress, err := c.ingressLister.Ingresses(namespaceKey).Get(name)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	if ok && errors.IsNotFound(err) {
+		// create ingress
+		ig := c.constructIngress(service)
+		_, err := c.client.NetworkingV1().Ingresses(namespaceKey).Create(context.TODO(), ig, v3.CreateOptions{})
+		if err != nil {
+			return err
+		}
+	} else if !ok && ingress != nil {
+		// delete ingress
+		err := c.client.NetworkingV1().Ingresses(namespaceKey).Delete(context.TODO(), name, v3.DeleteOptions{})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
+
+func (c *controller) handlerError(key string, err error) {
+	if c.queue.NumRequeues(key) <= maxRetry {
+		c.queue.AddRateLimited(key)
+		return
+	}
+	runtime.HandleError(err)
+	c.queue.Forget(key)
+
+}
+
+func (c *controller) constructIngress(service *v4.Service) *v2.Ingress {
+	ingress := v2.Ingress{}
+
+	ingress.ObjectMeta.OwnerReferences = []v3.OwnerReference{
+		*v3.NewControllerRef(service, v4.SchemeGroupVersion.WithKind("Service")),
+	}
+
+	ingress.Name = service.Name
+	ingress.Namespace = service.Namespace
+	pathType := v2.PathTypePrefix
+	icn := "nginx"
+	ingress.Spec = v2.IngressSpec{
+		IngressClassName: &icn,
+		Rules: []v2.IngressRule{
+			{
+				Host: "example.com",
+				IngressRuleValue: v2.IngressRuleValue{
+					HTTP: &v2.HTTPIngressRuleValue{
+						Paths: []v2.HTTPIngressPath{ // 移除多余的 {}
+							{
+								Path:     "/",
+								PathType: &pathType,
+								Backend: v2.IngressBackend{
+									Service: &v2.IngressServiceBackend{ // 添加 v2. 前缀
+										Name: service.Name,
+										Port: v2.ServiceBackendPort{ // 添加 v2. 前缀
+											Number: 80,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				Host: "localhost",
+				IngressRuleValue: v2.IngressRuleValue{
+					HTTP: &v2.HTTPIngressRuleValue{
+						Paths: []v2.HTTPIngressPath{ // 移除多余的 {}
+							{
+								Path:     "/",
+								PathType: &pathType,
+								Backend: v2.IngressBackend{
+									Service: &v2.IngressServiceBackend{ // 添加 v2. 前缀
+										Name: service.Name,
+										Port: v2.ServiceBackendPort{ // 添加 v2. 前缀
+											Number: 80,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return &ingress
 }
 
 func NewController(client kubernetes.Interface, serviceInformer informer.ServiceInformer, ingressInformer netInformer.IngressInformer) controller {
